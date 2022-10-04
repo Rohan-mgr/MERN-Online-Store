8f7ed90ba0074f299414c16d025a3fe25b5593e3
Rohan Magar <rohan.magar.415@gmail.com>
2022-10-04 20:47:23 +0545

    implemented diplaying the user orders

--- client/src/Components/Pages/Orders/Orders.js
+++ client/src/Components/Pages/Orders/Orders.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./Orders.css";
 
 function Orders(props) {
   const [orderedItems, setOrderedItems] = useState([]);
@@ -25,7 +27,34 @@ function Orders(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   console.log(orderedItems);
-  return <div>Order items</div>;
+  return (
+    <Container>
+      {orderedItems.length > 0 ? (
+        <ul className="orders">
+          {orderedItems.map((p) => {
+            return (
+              <li className="orders__item">
+                <h2>
+                  Order - # {p._id} - <a>Invoice</a>
+                </h2>
+                <ul class="orders__products">
+                  {p.products.map((i) => {
+                    return (
+                      <li class="orders__products__item">
+                        {i.product.title} ({i.quantity})
+                      </li>
+                    );
+                  })}
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <h2 className="text-center">No products available!</h2>
+      )}
+    </Container>
+  );
 }
 
 export default Orders;
